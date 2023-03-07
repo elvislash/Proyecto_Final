a2c31442d12d17fe81caba403b7f0d2be688553f
ana <118771627+elvislash@users.noreply.github.com>
2023-03-07 14:35:54 -0300

    commit 3

--- Proyecto_Final/src/app/app-routing.module.ts
+++ Proyecto_Final/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes), HeaderComponent ],
+  exports: [ RouterModule ],
+  declarations: [ HeaderComponent ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppRoutingModule { }
